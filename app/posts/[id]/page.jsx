993c1a5d5b0edcdd41e32ac09e5acd1cfb6b7ec1
Zehan Khan <zehan9211@gmail.com>
2022-11-07 15:33:28 +0530

    next revalidate and cache no-store option in fetch

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,5 +1,23 @@
-export default function PostWithIdPage( { params, searchParams } ){
+import { Fragment } from "react";
+
+const getPostById = async (id) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, 
+        {
+            // next: { revalidate: 10 },
+            cache: 'no-store'
+
+        }
+    )
+    const post = await response.json();
+    return post;
+}
+
+export default async function PostWithIdPage({ params, searchParams }) {
+    const post = await getPostById(params.id)
     return (
-        <div>Post Id Page</div>
+        <Fragment>
+            <div>Post Id Page</div>
+            <pre>{JSON.stringify(post, null, 2)}</pre>
+        </Fragment>
     )
 }
